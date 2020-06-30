50a9594450fdfa40e157cf6ae6db5fe652f53ccb
Shane Koehler <shane.koehler@gmail.com>
2020-06-30 12:30:34 -0500

    class structure

--- db/db.js
+++ db/db.js
@@ -1,8 +1,9 @@
 
 /** db class for managing note database file
+ *  uses index as ID for deleting
  * 
  */
-class db
+class db_note
 {
     /** constructor of db class
      * 
@@ -13,7 +14,33 @@ class db
         // make file if doesn't exist
 
         this.fileName = fileName;
+        this.data = [];
+    }
+
+    addNote(title, note)
+    {
+        // appends note to object array
+    }
+
+    deleteNote(index)
+    {
+        // removes note by index/id
+    }
+
+    getNotes(limit = null, pageNumber = 1)
+    {
+
+    }
+
+    saveDatabase()
+    {
+        // convert and write objArray to this.fileName
+    }
+
+    loadDatabase()
+    {
+        // read this.fileName and convert into object array and set this.data
     }
 }
 
-module.exports = db;
+module.exports = db_note;
